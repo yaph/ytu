70438b224da4e0bb65ab4e3fe6541fb413a9ffb4
Ramiro Gómez <code@ramiro.org>
2019-07-27 14:48:04 +0200

    Remove bumpversion, it either doesn't work or is not configured properly

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 """The setup script."""
 
 from setuptools import setup, find_packages
+from ytu import __version__
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -45,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/yaph/ytu',
-    version='version='0.1.0'',
+    version=__version__,
     zip_safe=False,
 )
